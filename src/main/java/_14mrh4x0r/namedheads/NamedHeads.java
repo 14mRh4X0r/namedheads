2ef4fd4ccc27df33e812a3380c9969f760fed334
Willem Mulder <willemmaster@hotmail.com>
2017-02-27 23:16:45 +0100

    Fix import order

--- src/main/java/_14mrh4x0r/namedheads/NamedHeads.java
+++ src/main/java/_14mrh4x0r/namedheads/NamedHeads.java
@@ -18,11 +18,12 @@ package _14mrh4x0r.namedheads;
 
 import org.lwjgl.opengl.GL11;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntitySkull;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
@@ -34,9 +35,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import java.lang.reflect.Field;
-import net.minecraft.client.renderer.RenderGlobal;
-import net.minecraft.init.Blocks;
-import scala.actors.threadpool.Arrays;
+import java.util.Arrays;
 
 @Mod(modid = NamedHeads.MODID, version = NamedHeads.VERSION)
 @SideOnly(Side.CLIENT)
