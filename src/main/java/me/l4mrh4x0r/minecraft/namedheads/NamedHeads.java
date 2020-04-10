ae3a173cfecc84b28d48d1c119984ca18bf3a17c
Willem Mulder <willemmaster@hotmail.com>
2020-04-10 21:38:00 +0200

    Move to me.l4mrh4x0r.minecraft namespace

--- src/main/java/me/l4mrh4x0r/minecraft/namedheads/NamedHeads.java
+++ src/main/java/me/l4mrh4x0r/minecraft/namedheads/NamedHeads.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package _14mrh4x0r.namedheads;
+package me.l4mrh4x0r.minecraft.namedheads;
 
 import org.lwjgl.opengl.GL11;
 
